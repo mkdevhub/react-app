f96d70cd0347c612984ab8147f16010c6c280177
Muthukrishnan <muthukrishnan@luminescent.digital>
2022-11-27 19:23:22 +0530

    setup git deploy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ function Header(props) {
     <div className="main-head">
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="./">
             <img src={logo} alt="Logo" />
           </a>
           <MainMenu menuList={menuData} />
