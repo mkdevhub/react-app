d02ad64f4861b60815a37636577f5950746eb4ef
Muthukrishnan <muthukrishnan@luminescent.digital>
2022-11-29 10:10:56 +0530

    Changes updated

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -3,8 +3,8 @@ import TodoCard from "../components/TodoCard/TodoCard";
 const Todo = () => {
   return (
     <div className="todo-wrap">
-      <h1>Todo App</h1>
-
+      <h1>Todo App - in-progress</h1>
+      <br></br>
       <TodoCard />
     </div>
   );
